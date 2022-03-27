34698789cd8d839f03d8ae4723e1e34bbb331495
davidbenm <dbenitezm92@gmail.com>
2022-03-27 20:42:11 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,8 +2,28 @@
 
 # Backing up required files
 
-echo "Creating backup directory." && mkdir ~/backup || echo "Directory already exists."
+LOGFILE=$1
+BACKUP_LOC="/usr/bin/"
+BACKUP_TARGET="/home/$USER/backup"
 
-echo "Copying files." && cp -r /usr/bin/* ~/backup
+init () {
+        echo "Timestamp before work is done: $(date +"%D %T")" > $LOGFILE
+        echo "Creating backup directory." >> $LOGFILE  && mkdir $BACKUP_TARGET 2> /dev/null || echo "Directory already exists." >> $LOGFILE
+}
 
-echo "Backup done!"
+init
+
+#echo "Copying files." >> $LOGFILE && cp -r $BACKUP_LOC $BACKUP_TARGET >> $LOGFILE 2>&1
+echo "Copying files." >> $LOGFILE
+cd $BACKUP_LOC
+for i in $(ls); do
+        cp -r "$i" $BACKUP_TARGET/"$i"-backup >> /home/$USER/$LOGFILE 2>&1
+done
+
+grep -i denied /home/$USER/$LOGFILE | tail -n 2 >> /home/$USER/$LOGFILE
+
+sleep 3
+
+echo "Backup ready!" >> /home/$USER/$LOGFILE
+echo "Timestamp after work is done: $(date +"%D %T")" >> /home/$USER/$LOGFILE
+echo "------------------------------------------------" >> /home/$USER/$LOGFILE
