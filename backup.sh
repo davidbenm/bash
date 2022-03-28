4df9f50584071004e67975f8107d6a56c1832d18
davidbenm <dbenitezm92@gmail.com>
2022-03-28 00:44:08 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,13 +2,27 @@
 
 # Backing up required files
 
+if [ -z $1 ]
+then
+        echo "You need to supply a parameter for the log file."
+        exit 255
+fi
+
 LOGFILE=$1
 BACKUP_LOC="/usr/bin/"
 BACKUP_TARGET="/home/$USER/backup"
 
 init () {
         echo "Timestamp before work is done: $(date +"%D %T")" > $LOGFILE
-        echo "Creating backup directory." >> $LOGFILE && mkdir $BACKUP_TARGET 2> /dev/null || echo "Directory already exists." >> $LOGFILE
+        if [ -d $BACKUP_TARGET ]
+        then
+                echo "Directory already exists." >> $LOGFILE
+                return 1
+        else
+                mkdir $BACKUP_TARGET
+                echo "Creating backup directory." >> $LOGFILE
+                return 0
+        fi
 }
 
 cleanup () {
@@ -18,19 +32,20 @@ cleanup () {
         exit
 }
 
+#set -x
 init
+#set +x
 trap cleanup SIGINT
 
 echo "Copying files." >> $LOGFILE
 cd $BACKUP_LOC
-for i in $(ls); do
-        cp -r "$i" $BACKUP_TARGET/"$i"-backup >> /home/$USER/$LOGFILE 2>&1
+for i in $(ls /usr/bin/ | grep -E 'deb|dpkg|docker'); do
+        cp -v -r "$i" $BACKUP_TARGET/"$i"-backup >> /home/$USER/$LOGFILE 2>&1
 done
 
 grep -i denied /home/$USER/$LOGFILE | tail -n 2 >> /home/$USER/$LOGFILE
 
-#sleep 3
-
 echo "Backup ready!" >> /home/$USER/$LOGFILE
+sleep 5
 echo "Timestamp after work is done: $(date +"%D %T")" >> /home/$USER/$LOGFILE
 echo "------------------------------------------------" >> /home/$USER/$LOGFILE
